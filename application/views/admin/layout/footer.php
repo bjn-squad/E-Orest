f4cb8650edb05e2b1557cd981ed4afceba88e740
mrizazul <rezazulfah007@gmail.com>
2021-09-14 15:03:00 +0700

    add template for admin

---
+++ application/views/admin/layout/footer.php
@@ -0,0 +1,43 @@
+<!-- Footer -->
+<footer class="footer pt-0">
+    <div class="row align-items-center justify-content-lg-between">
+        <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+                &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                <li class="nav-item">
+                    <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+<!-- Argon Scripts -->
+<!-- Core -->
+<script src="<?= base_url() ?>assets/admin/vendor/jquery/dist/jquery.min.js"></script>
+<script src="<?= base_url() ?>assets/admin/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>assets/admin/vendor/js-cookie/js.cookie.js"></script>
+<script src="<?= base_url() ?>assets/admin/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+<script src="<?= base_url() ?>assets/admin/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+<!-- Optional JS -->
+<script src="<?= base_url() ?>assets/admin/vendor/chart.js/dist/Chart.min.js"></script>
+<script src="<?= base_url() ?>assets/admin/vendor/chart.js/dist/Chart.extension.js"></script>
+<!-- Argon JS -->
+<script src="<?= base_url() ?>assets/admin/js/argon.js?v=1.2.0"></script>
+</body>
+
+</html>
---
+++ application/views/admin/layout/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
+    <!-- Favicon -->
+    <link rel="icon" href="<?= base_url() ?>assets/admin/img/brand/favicon.png" type="image/png">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+    <!-- Icons -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/admin/vendor/nucleo/css/nucleo.css" type="text/css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/admin/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+    <!-- Page plugins -->
+    <!-- Argon CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/admin/css/argon.css?v=1.2.0" type="text/css">
+</head>
