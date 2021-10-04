7a227434a68e5c7c7b4d566f4490fd34ebd22f78
Ardan Anjung Kusuma <ardananjungkusuma@gmail.com>
2021-10-04 21:04:40 +0700

    Migrating to new Template and add dynamic profil usaha feature (#12)
    * Fix some layout and fix login ui
    
    * Add logout
    
    * Add Tambah Meja and Datatables
    
    * Add menu
    
    * Fix add, delete gambar
    
    * Update menu
    
    * Adding table feature CRUD
    
    * add CRUD Menu
    
    * add DB Menu
    
    * Migrating to new Template and Add Profil Usaha
    
    Co-authored-by: Dinalis <lisuardidina@gmail.com>

--- application/views/home/layout/header.php
+++ application/views/home/layout/header.php
@@ -5,70 +5,54 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Restaurantly Bootstrap Template - Index</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+  <title><?= $nama_usaha ?></title>
 
   <!-- Favicons -->
-  <link href="<?= base_url() ?>assets/home/img/favicon.png" rel="icon">
-  <link href="<?= base_url() ?>assets/home/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?= base_url() ?><?= base_url() ?>assets/home/home/img/favicon.png" rel="icon">
+  <link href="<?= base_url() ?><?= base_url() ?>assets/home/home/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?= base_url() ?>assets/home/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/home/vendor/aos/aos.css" rel="stylesheet">
   <link href="<?= base_url() ?>assets/home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?= base_url() ?>assets/home/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="<?= base_url() ?>assets/home/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="<?= base_url() ?>assets/home/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/home/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="<?= base_url() ?>assets/home/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="<?= base_url() ?>assets/home/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: Restaurantly - v3.3.0
-  * Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
 
-  <!-- ======= Top Bar ======= -->
-  <div id="topbar" class="d-flex align-items-center fixed-top">
-    <div class="container d-flex justify-content-center justify-content-md-between">
-
-      <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-phone d-flex align-items-center"><span>+1 5589 55488 55</span></i>
-        <i class="bi bi-clock d-flex align-items-center ms-4"><span> Mon-Sat: 11AM - 23PM</span></i>
-      </div>
-
-
-    </div>
-  </div>
-
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-cente">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo me-auto me-lg-0"><a href="index.html">Restaurantly</a></h1>
+      <h1 class="logo"><a href="<?= base_url() ?>"><?= $nama_usaha ?></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="<?= base_url() ?>assets/home/img/logo.png" alt="" class="img-fluid"></a>-->
+      <!-- <a href="index.html" class="logo"><img src="<?= base_url() ?>assets/home/img/logo.png" alt="" class="img-fluid"></a>-->
 
-      <nav id="navbar" class="navbar order-last order-lg-0">
+      <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto" href="<?= base_url() ?>">Home</a></li>
-          <li><a class="nav-link scrollto" href="<?= base_url() ?>home/menu">Katalog Menu</a></li>
-          <li><a class="nav-link scrollto" href="<?= base_url() ?>home/pembayaran">Riwayat Pembayaran</a></li>
-          <li><a class="nav-link scrollto" href="<?= base_url() ?>saran/add">Kritik & Saran</a></li>
+          <li><a class="active" href="<?= base_url() ?>">Home</a></li>
+          <li><a href="<?= base_url() ?>home/menu">Katalog Menu</a></li>
+          <li><a href="<?= base_url() ?>home/booking">Booking Sekarang</a></li>
+          <li><a href="<?= base_url() ?>home/pembayaran">Cek Pembayaran</a></li>
+          <li><a href="<?= base_url() ?>saran/add">Kritik & Saran</a></li>
+          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li><a href="#">Drop Down 2</a></li>
+            </ul>
+          </li> -->
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
-      <a href="<?= base_url() ?>home/booking" class="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a>
 
     </div>
   </header><!-- End Header -->
