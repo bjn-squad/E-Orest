f81d858f7ce58569bb756abf8dc370d89f704e32
Ardan Anjung Kusuma <ardananjungkusuma@gmail.com>
2021-10-24 15:08:35 +0700

    Add fitur edit profil usaha

--- application/models/Profilusaha_model.php
+++ application/models/Profilusaha_model.php
@@ -49,4 +49,99 @@ class Profilusaha_model extends CI_Model
         $this->db->where('id_metode', $id);
         $this->db->delete('metode_pembayaran');
     }
+
+    public function edit_profil_usaha()
+    {
+        $pathFUsaha = "assets/dataresto/foto_usaha/";
+        $current_usaha = $this->input->post('current_id');
+        $getDataGambar = $this->db->query("SELECT * FROM profil_usaha WHERE id = $current_usaha");
+
+        foreach ($getDataGambar->result_array() as $gambar) {
+            $gambar1_old = $gambar['foto_usaha_1'];
+            $gambar2_old = $gambar['foto_usaha_2'];
+            $gambar3_old = $gambar['foto_usaha_3'];
+        }
+
+        if ($_FILES['foto_usaha_1']['size'] != 0) {
+            $file_name1 = $_FILES['foto_usaha_1']['name'];
+            $newfile_name1 = str_replace(' ', '', $file_name1);
+            $config['upload_path']          = './assets/dataresto/foto_usaha';
+            $newName = date('dmYHis') .  $newfile_name1;
+            $config['file_name']         = $newName;
+            $config['max_size']             = 10100;
+            $config['allowed_types']        = 'jpg|png|jpeg';
+            $this->load->library('upload', $config);
+            $this->upload->initialize($config);
+            if ($this->upload->do_upload('foto_usaha_1')) {
+                if ($gambar1_old != "1.jpg") {
+                    unlink($pathFUsaha . $gambar1_old);
+                }
+                $data = [
+                    'foto_usaha_1' => $newName
+                ];
+
+                $this->db->where('id', $current_usaha);
+                $this->db->update('profil_usaha', $data);
+            }
+        }
+
+        if ($_FILES['foto_usaha_2']['size'] != 0) {
+            $file_name2 = $_FILES['foto_usaha_2']['name'];
+            $newfile_name2 = str_replace(' ', '', $file_name2);
+            $config1['upload_path']          = './assets/dataresto/foto_usaha';
+            $newName1 = date('dmYHis') .  $newfile_name2;
+            $config1['file_name']         = $newName1;
+            $config1['max_size']             = 10100;
+            $config1['allowed_types']        = 'jpg|png|jpeg';
+            $this->load->library('upload', $config1);
+            $this->upload->initialize($config1);
+            if ($this->upload->do_upload('foto_usaha_2')) {
+                if ($gambar2_old != "2.jpg") {
+                    unlink($pathFUsaha . $gambar2_old);
+                }
+                $data = [
+                    'foto_usaha_2' => $newName1
+                ];
+
+                $this->db->where('id', $current_usaha);
+                $this->db->update('profil_usaha', $data);
+            }
+        }
+
+        if ($_FILES['foto_usaha_3']['size'] != 0) {
+            $file_name3 = $_FILES['foto_usaha_3']['name'];
+            $newfile_name3 = str_replace(' ', '', $file_name3);
+            $config2['upload_path']          = './assets/dataresto/foto_usaha';
+            $newName2 = date('dmYHis') .  $newfile_name3;
+            $config2['file_name']         = $newName2;
+            $config2['max_size']             = 10100;
+            $config2['allowed_types']        = 'jpg|png|jpeg';
+            $this->load->library('upload', $config2);
+            $this->upload->initialize($config2);
+            if ($this->upload->do_upload('foto_usaha_3')) {
+                if ($gambar3_old != "3.jpg") {
+                    unlink($pathFUsaha . $gambar3_old);
+                }
+                $data = [
+                    'foto_usaha_3' => $newName2
+                ];
+
+                $this->db->where('id', $current_usaha);
+                $this->db->update('profil_usaha', $data);
+            }
+        }
+
+        $data = [
+            'nama_usaha' => htmlspecialchars($this->input->post('nama_usaha', true)),
+            'alamat' => htmlspecialchars($this->input->post('alamat', true)),
+            'nomor_telepon' => htmlspecialchars($this->input->post('nomor_telepon', true)),
+            'email' => htmlspecialchars($this->input->post('email', true)),
+            'instagram' => htmlspecialchars($this->input->post('instagram', true)),
+            'facebook' => htmlspecialchars($this->input->post('facebook', true)),
+            'maps_link' => $this->input->post('maps_link')
+        ];
+
+        $this->db->where('id', $current_usaha);
+        $this->db->update('profil_usaha', $data);
+    }
 }
---
+++ application/views/admin/profil_usaha/edit.php
@@ -0,0 +1,91 @@
+<div class="header bg-primary pb-6">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="row align-items-center py-4">
+                <div class="col-lg-6 col-7">
+                    <h6 class="h2 text-white d-inline-block mb-0">Dashboard</h6>
+                    <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                        <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                            <li class="breadcrumb-item"><a href="<?= base_url() ?>admin"><i class="fas fa-home"></i></a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Profil Usaha</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page content -->
+<div class="container-fluid mt--6">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <!-- Card header -->
+                <div class="card-header border-0">
+                    <h3 class="mb-0">Profil Usaha Anda</h3>
+                </div>
+                <div class="col-lg-12">
+                    <?= $this->session->flashdata('message'); ?>
+                    <?php foreach ($profil_usaha as $ps) {
+                    ?>
+                        <div class="card-body">
+                            <?= $this->session->flashdata('message'); ?>
+                            <div class="form-group">
+                                <form action="<?= base_url() ?>profilusaha/edit" method="POST" enctype="multipart/form-data">
+                                    <input type="text" name="current_id" required value="<?= $ps['id'] ?>">
+                                    <label>Nama Usaha</label>
+                                    <input type="text" class="form-control" name="nama_usaha" value="<?= $ps['nama_usaha'] ?>" required>
+                                    <label>Alamat</label>
+                                    <input type="text" class="form-control" name="alamat" value="<?= $ps['alamat'] ?>" required>
+                                    <label>Nomor Telepon</label>
+                                    <input type="number" class="form-control" name="nomor_telepon" value="<?= $ps['nomor_telepon'] ?>" required>
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" name="email" value="<?= $ps['email'] ?>">
+                                    <label>Instagram</label><small>* Tidak usah diberi @</small>
+                                    <input type="text" class="form-control" name="instagram" value="<?= $ps['instagram'] ?>">
+                                    <label>Facebook</label>
+                                    <input type="text" class="form-control" name="facebook" value="<?= $ps['facebook'] ?>">
+                                    <label>Maps Link</label><small>* Cara mengambil link maps? Klik disini</small>
+                                    <textarea type="text" rows="5" class="form-control" name="maps_link"><?= $ps['maps_link'] ?></textarea>
+                            </div>
+                            <label class="font-weight-bold">Gambar Slide Show Restoran pada Homepage:</label><br>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="text-center">
+                                        <img class="m-3" src="<?= base_url() ?>assets/dataresto/foto_usaha/<?= $ps['foto_usaha_1'] ?>" width="100%"><br>
+                                        Gambar 1
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="text-center">
+                                        <img class="m-3" src="<?= base_url() ?>assets/dataresto/foto_usaha/<?= $ps['foto_usaha_2'] ?>" width="100%"><br>
+                                        Gambar 2
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="text-center">
+                                        <img class="m-3" src="<?= base_url() ?>assets/dataresto/foto_usaha/<?= $ps['foto_usaha_3'] ?>" width="100%"><br>
+                                        Gambar 3
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <small>* Jika tidak ingin mengganti gambar silahkan kosongi.</small><br>
+                                <label for="exampleFormControlFile1">Gambar 1</label>
+                                <input type="file" class="form-control-file" name="foto_usaha_1" accept=".jpg, .jpeg, .png">
+                                <label for="exampleFormControlFile1">Gambar 2</label>
+                                <input type="file" class="form-control-file" name="foto_usaha_2" accept=".jpg, .jpeg, .png">
+                                <label for="exampleFormControlFile1">Gambar 3</label>
+                                <input type="file" class="form-control-file" name="foto_usaha_3" accept=".jpg, .jpeg, .png">
+                                <button type="submit" class="btn btn-primary mt-3">Edit Profil Usaha</button>
+                                </form>
+                            </div>
+                        </div>
+                    <?php
+                    } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
